66e01c0f938fb2db65c6811acf07ea1121028b10
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2024-07-17 16:23:01 -0600

    Create player stat crud

---
+++ app/Http/Controllers/PlayerStatController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class PlayerStatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return PlayerStat::all();
+    }
+
+    public function store(Request $request)
+    {
+        // Validate the request data as needed
+        $request->validate([
+            'heartbeat' => 'required|integer',
+            'player_id' => 'required|string',
+        ]);
+
+
+        PlayerStat::create([
+            'heartbeat' => $request->input('heartbeat'),
+            'level_id' => $request->input('level_id', null),
+            'player_id' => $request->input('player_id'),
+            'play_session_count' => $request->input('play_session_count', null),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        // Optionally, you can return a response to indicate success
+        return response()->json(['message' => 'Heartbeat record created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return PlayerStat::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        PlayerStat::where('id', $id)
+            ->update([
+                'player_score' => $request->input('player_score', null),
+                'variation' => $request->input('variation', null),
+            ]);
+
+        // Optionally, you can return a response to indicate success
+        return response()->json(['message' => 'Heartbeat record updated successfully'], 201);
+    }
+
+
+}
---
+++ database/migrations/2024_07_17_212304_create_player_stats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('player_stats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('heartbeat');
+            $table->string('player_id');
+            $table->integer('play_session_count');
+            $table->string('level_id');
+            $table->string('version_id');
+            $table->string('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('player_stats');
+    }
+};
