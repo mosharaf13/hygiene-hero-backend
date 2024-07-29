23ec2a888a5f230c1e4d90c456037aea7a157a87
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2024-07-29 14:46:05 -0600

    Add heartbeat submit url

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::get('/', function () {
 });
 
 Route::get('/heartbeats', [App\Http\Controllers\PlayerStatController::class, 'index'])->name('heartbeats.index');
+Route::get('/heartbeats/submit', [App\Http\Controllers\PlayerStatController::class, 'store'])->name('heartbeats.submit');
 Route::post('/heartbeats', [App\Http\Controllers\PlayerStatController::class, 'store'])->name('heartbeats.store');
 Route::put('/heartbeats', [App\Http\Controllers\PlayerStatController::class, 'update'])->name('heartbeats.update');
