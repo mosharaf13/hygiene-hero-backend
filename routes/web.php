303aa6e749b6855360c853c7f9a36faf295bcd4c
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2024-07-31 13:39:29 -0600

    Add route to fetch latest heartbeat

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
 });
 
 Route::get('/player-stats', [App\Http\Controllers\PlayerStatController::class, 'index'])->name('player-stats.index');
+Route::get('/player-stats/latest', [App\Http\Controllers\PlayerStatController::class, 'latest'])->name('player-stats.latest');
 Route::get('/player-stats/submit', [App\Http\Controllers\PlayerStatController::class, 'store'])->name('player-stats.submit');
 Route::post('/player-stats', [App\Http\Controllers\PlayerStatController::class, 'store'])->name('player-stats.store');
 Route::put('/player-stats', [App\Http\Controllers\PlayerStatController::class, 'update'])->name('player-stats.update');
