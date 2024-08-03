928fafd61b6598a39da0fe21a8d6c51756c035f4
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2024-08-02 18:45:15 -0600

    Add heartbeat threshold and next player id api

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerStatController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/player-stats', [App\Http\Controllers\PlayerStatController::class, 'index'])->name('player-stats.index');
-Route::get('/player-stats/latest', [App\Http\Controllers\PlayerStatController::class, 'latest'])->name('player-stats.latest');
-Route::get('/player-stats/submit', [App\Http\Controllers\PlayerStatController::class, 'store'])->name('player-stats.submit');
-Route::post('/player-stats', [App\Http\Controllers\PlayerStatController::class, 'store'])->name('player-stats.store');
-Route::put('/player-stats', [App\Http\Controllers\PlayerStatController::class, 'update'])->name('player-stats.update');
+Route::get('/player-stats', [PlayerStatController::class, 'index'])->name('player-stats.index');
+Route::get('/heartbeat-threshold/{playerId}', [PlayerStatController::class, 'threshold']);
+Route::get('/player-stats/latest', [PlayerStatController::class, 'latest'])->name('player-stats.latest');
+Route::get('/player-stats/submit', [PlayerStatController::class, 'store'])->name('player-stats.submit');
+Route::post('/player-stats', [PlayerStatController::class, 'store'])->name('player-stats.store');
+Route::put('/player-stats', [PlayerStatController::class, 'update'])->name('player-stats.update');
+
+Route::get('/player-stats/next-player-id', [PlayerStatController::class, 'getNextPlayerId'])->name('player-stats.next-player-id');
